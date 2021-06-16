789b158a14ac31c04f5a041d418b56a5a5d9e347
llSeeSharpll <79083381+llSeeSharpll@users.noreply.github.com>
2021-06-16 11:48:34 +0300

    all files

---
+++ backend/src/main/java/com/example/backend/repository/TutorialRepository.java
@@ -0,0 +1,13 @@
+package com.example.backend.repository;
+
+import java.util.List;
+
+import com.example.backend.model.Tutorial;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+    List<Tutorial> findByPublished(boolean published);
+    List<Tutorial> findByTitleContaining(String title);
+}
